008725260819a009f93f0dbcc1e7ca95a865d949
polenom <vitalimitsevich@yandex.by>
2022-04-04 19:46:44 +0300

    feat: add PUT and DELETE

--- pjapi/serializer.py
+++ pjapi/serializer.py
@@ -4,9 +4,33 @@ from photojournal.models import *
 class BlogsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
-        fields = ('photo', 'description', 'creation', 'user', 'title','slug')
-
+        fields = ('photo', 'description', 'user', 'title','slug')
 
     def create(self, validated_data):
-
         return Blog.objects.create(**validated_data)
+
+
+class BlogUpdateSerializer(serializers.Serializer):
+    title = serializers.CharField(read_only=True)
+    # photo = serializers.ImageField()
+    slug = serializers.SlugField()
+    description = serializers.CharField(read_only=True)
+
+    def update(self, instance, validated_data):
+        print('start')
+        # instance.photo = invalidated_data.get("photo", instance.photo)
+        instance.description = validated_data.get("description", instance.description)
+        instance.title = validated_data.get("title", instance.title)
+        instance.slug = validated_data.get("slug", instance.slug)
+        print('finish')
+        instance.save()
+        return instance
+
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Имя пользователя', related_name='blogs')
+    # photo = models.ImageField(upload_to=photo_path, verbose_name='Фото')
+    # description = models.TextField(max_length=100, verbose_name='Описание')
+    # creation = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+    # photoPublish = models.BooleanField(default=True, verbose_name='Статус')
+    # likesBlog = models.ManyToManyField(User, blank=True, related_name='likes')
+    # title = models.CharField(max_length=40, unique=True)
+    # slug = models.SlugField(null=True)
--- pjapi/views.py
+++ pjapi/views.py
@@ -6,7 +6,7 @@ from  photojournal.models import Blog
 
 from photojournal.models import *
 # Create your views here.
-from pjapi.serializer import BlogsSerializer
+from pjapi.serializer import BlogsSerializer, BlogUpdateSerializer
 
 
 class BlogsAPIView(APIView):
@@ -22,10 +22,40 @@ class BlogsAPIView(APIView):
             request.data['user'] = User.objects.get(username = request.data['user']).pk
         except User.DoesNotExist:
             return Response({'blog': '', 'result': 'false'})
+        print(request.data['user'],123333333333333)
         serializer = BlogsSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({'blog':serializer.data, 'result':'true'})
 
 
+    def put(self, request):
+        slug = request.data.get('slug', None)
 
+
+        if not slug:
+            return Response({'error':"Method PUT don't allow"})
+
+        try:
+            a = Blog.objects.get(slug=slug)
+        except Blog.DoesNotExist:
+            return Response({'error':'object does not exists'})
+
+        serializer = BlogUpdateSerializer(instance=a, data=request.data)
+        print(request.data,3333333333333)
+        serializer.is_valid(raise_exception=True)
+        # if serializer.is_valid():
+        #     return Response({'blog': '', 'status': 'false'})
+        serializer.save()
+        return Response({'blog': serializer.data, 'status': 'true'})
+
+    def delete(self, request):
+        slug = request.data.get('slug', None)
+        if not slug:
+            return Response({'error':'Method DELETE not allowed'})
+        try:
+            res = Blog.objects.get(slug=slug)
+        except Blog.DoesNotExist:
+            return Response({'result': 'false'})
+        res.delete()
+        return Response({'result': 'true'})
