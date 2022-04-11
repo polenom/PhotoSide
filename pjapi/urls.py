f7c1424af988fee9841b5a8d53797712a7fa1ccc
polenom <vitalimitsevich@yandex.by>
2022-04-11 21:10:11 +0300

    feat:add jwt token

--- pjapi/urls.py
+++ pjapi/urls.py
@@ -2,6 +2,11 @@ from django.urls import path, include
 from rest_framework import routers
 
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 router = routers.DefaultRouter()
@@ -16,7 +21,11 @@ print(router.urls)
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/auth-sid/', include('rest_framework.urls')),
-    path('v1/token/', GetToken.as_view())
+    path('v1/token/', GetToken.as_view()),
+    path('v1/jwt/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/jwt/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/jwt/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
     # path('v1/blog/', BlogViewSet.as_view({'get':'list', 'post':'create'})),
     # path('v1/blog/<int:pk>', BlogViewSet.as_view({'put':'update', 'delete':'destroy'}))
 ]
